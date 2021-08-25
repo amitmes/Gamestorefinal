8d8d0774865d6dbb8c373c86db3d2ef72b5ed9eb
amitmes <amitmes11@gmail.com>
2021-08-25 14:48:37 +0300

    graph

--- Gamestorefinal/wwwroot/js/graphs.js
+++ Gamestorefinal/wwwroot/js/graphs.js
@@ -1,11 +1,11 @@
 ﻿
-var data = [{ name: 'John', score: 80}];
+var data = [{ name: 'John', score: 80 }];
 
 
 
 $.ajax({
     url: '/Home/Datafromdb'
-}).then(function (data1) {
+}).done(function (data1) {
     $.each(data1, function (i, val) {
         $.each(val, function (key, value) {
             if (key.valueOf().match("name")) {
@@ -18,56 +18,56 @@ $.ajax({
 
         });
     });
-    const width = 1800;
-    const height = 450;
-    const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+    var margin = { top: 20, right: 20, bottom: 30, left: 40 },
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
 
-    const svg = d3.select('#d3-container')
-        .append('svg')
-        .attr('width', width - margin.left - margin.right)
-        .attr('height', height - margin.top - margin.bottom)
-        .attr("viewBox", [0, 0, width, height]);
+    // set the ranges
+    var y = d3.scaleBand()
+        .range([height, 0])
+        .padding(0.1);
 
-    const x = d3.scaleBand()
-        .domain(d3.range(data.length))
-        .range([margin.left, width - margin.right])
-        .padding(0.5)
+    var x = d3.scaleLinear()
+        .range([0, width]);
 
-    const y = d3.scaleLinear()
-        .domain([0, 100])
-        .range([height - margin.bottom, margin.top])
-
-    svg
+    // append the svg object to the body of the page
+    // append a 'group' element to 'svg'
+    // moves the 'group' element to the top left margin
+    var svg = d3.select(".names").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
         .append("g")
-        .attr("fill", 'royalblue')
-        .selectAll("rect")
-        .data(data.sort((a, b) => d3.descending(a.score, b.score)))
-        .join("rect")
-        .attr("x", (d, i) => x(i))
-        .attr("y", d => y(d.score))
-        .attr('title', (d) => d.score)
-        .attr("class", "rect")
-        .attr("height", d => y(0) - y(d.score))
-        .attr("width", x.bandwidth())
-        .attr('class', 'rectangle')
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
+    // format the data
+    data.forEach(function (d) {
+        d.score = +d.score;
+    });
 
-    function yAxis(g) {
-        g.attr("transform", `translate(${margin.left}, 0)`)
-            .call(d3.axisLeft(y).ticks(null, data.format))
-            .attr("font-size", '20px')
-    }
+    // Scale the range of the data in the domains
+    x.domain([0, d3.max(data, function (d) { return d.score; })])
+    y.domain(data.map(function (d) { return d.name; }));
+    //y.domain([0, d3.max(data, function(d) { return d.sales; })]);
 
-    function xAxis(g) {
-        g.attr("transform", `translate(0,${height - margin.bottom})`)
-            .call(d3.axisBottom(x).tickFormat(i => data[i].name))
-            .attr("font-size", '20px')
-    }
+    // append the rectangles for the bar chart
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        //.attr("x", function(d) { return x(d.sales); })
+        .attr("width", function (d) { return x(d.score); })
+        .attr("y", function (d) { return y(d.name); })
+        .attr("height", y.bandwidth());
 
+    // add the x Axis
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-    svg.append("g").call(xAxis);
-    svg.append("g").call(yAxis);
-    svg.node();
+    // add the y Axis
+    svg.append("g")
+        .call(d3.axisLeft(y));
 });
 
 
